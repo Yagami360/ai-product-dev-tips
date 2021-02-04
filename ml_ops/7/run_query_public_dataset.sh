adbce4e1bba4db6fe38f9c143ba720588a45d14a
Yagami360 <y034112@gmail.com>
2021-02-04 20:22:27 +0900

    a

--- ml_ops/7/run_query_public_dataset.sh
+++ ml_ops/7/run_query_public_dataset.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -eu
+PROJECT_ID=bigquery-public-data
+DATASET_ID=usa_names
+TABLE_ID=usa_1910_2013
+
+bq show ${PROJECT_ID}:${DATASET_ID}.${TABLE_ID}
 
 bq query --use_legacy_sql=false \
     'SELECT
