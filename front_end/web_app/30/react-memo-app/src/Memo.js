8353c09cd40e0913dcf6a940e0412714a97aad0e
Yagami360 <y034112@gmail.com>
2021-10-22 00:08:30 +0900

    a

--- front_end/web_app/30/react-memo-app/src/Memo.js
+++ front_end/web_app/30/react-memo-app/src/Memo.js
@@ -8,13 +8,12 @@ class Memo extends Component {
   }
 
   render() {
-    console.log("this.props.data_list", this.props.data_list)
-    console.log("this.props.data_list[0]", this.props.data_list[0])
-    console.log("this.props.data_list[0].memo_text", this.props.data_list[0].memo_text)
-    console.log("this.props.data_list[0].created_time", this.props.data_list[0].created_time)
-
+    //console.log("this.props.data_list", this.props.data_list)
+    //console.log("this.props.data_list[0]", this.props.data_list[0])
+    //console.log("this.props.data_list[0].memo_text", this.props.data_list[0].memo_text)
+    //console.log("this.props.data_list[0].created_time", this.props.data_list[0].created_time)
     return (
-      <p>{this.props.data_list[0].created_time.getHours() + ':' + this.props.data_list[0].created_time.getMinutes() + ':' + this.props.data_list[0].created_time.getSeconds()}</p>
+      <p>テキスト : {this.props.data_list[0].memo_text}, 保存時間 : {this.props.data_list[0].created_time.getHours() + ':' + this.props.data_list[0].created_time.getMinutes() + ':' + this.props.data_list[0].created_time.getSeconds()}</p>
     );
   }
 }
